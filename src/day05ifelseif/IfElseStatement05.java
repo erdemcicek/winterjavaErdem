6b3b6006c664ae27ea7f5b123dfdc0c86330449b
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-05-20 16:19:21 +0200

    Update IfElseStatement05.java

--- src/day05ifelseif/IfElseStatement05.java
+++ src/day05ifelseif/IfElseStatement05.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class IfElseStatement05 {
 	/*
 	 * Type java code by using if-else if() statement.
-A school has following rules for grading system:
-1. Below 50 - D     2. 50 to 59 - C       3. 60 to 79 - B.     4. From 80 to 100 - A
-Ask user to enter marks and print the corresponding grade.
+	A school has following rules for grading system:
+	1. Below 50 - D     2. 50 to 59 - C       3. 60 to 79 - B.     4. From 80 to 100 - A
+	Ask user to enter marks and print the corresponding grade.
 	 */
 
 	public static void main(String[] args) {
@@ -25,7 +25,5 @@ Ask user to enter marks and print the corresponding grade.
 		else
 			System.out.println("Your grade is D");
 		scan.close();
-
 	}
-
 }
